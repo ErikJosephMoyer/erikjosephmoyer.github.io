7eb8694c920581f2f99ff536563e7a585efbd2e9
Erik Moyer <erik.moyer@goenumerate.com>
2026-05-04 22:00:27 -0500

    musicker: add mic record + URL fetch + live pitch monitor tab

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker for Erik's Tools PWA
 // Bump CACHE_VERSION to invalidate old caches when shipping updates.
-const CACHE_VERSION = 'em-tools-v38';
+const CACHE_VERSION = 'em-tools-v39';
 const APP_SHELL = [
   '/',
   '/sync.js',
